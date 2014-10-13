6338faf2aa185d4b3ed13954069637562aa9b350
Gerry Gonzaga <g@gdgon.com>
2014-10-14 01:05:58 +0800

    lock-window.lisp now works.

--- jaganet-lock-window/lock-window.lisp
+++ jaganet-lock-window/lock-window.lisp
@@ -1,29 +1,28 @@
 (ql:quickload 'ltk)
+(use-package 'ltk)
 (ql:quickload 'bordeaux-threads)
 
-(defun interrupt-thread-by-name (thread-name)
-  (handler-case (bt:interrupt-thread (find thread-name
-                                           (bt:all-threads)
-                                           :test #'string-equal
-                                           :key #'bt:thread-name)
-                                     #'(lambda ()
-                                         (signal 'shutting-down)))
-    (type-error () nil)))
-
 (defun show-idle-window ()
-  (handler-case
-    (with-ltk ()
-        (let ((image (make-image)))
-          (image-load image "bg.gif")
-          (let ((canvas (make-instance 'canvas)))
-            (create-image canvas 0 0 :image image)
-            (configure canvas :width 1600)
-            (configure canvas :height 900)
-            (pack canvas))))
-    (shutting-down () )))
+  (ignore-errors
+    (start-wish)
+    (set-wm-overrideredirect *tk* 1)
+    (set-geometry-xy *tk* 0 0)
+    (defparameter image (make-image))
+    ;(let ((image (make-image)))
+    (image-load image "bg.gif")
+    (defparameter canvas (make-instance 'canvas))
+      ;(let ((canvas (make-instance 'canvas)))
+    (create-image canvas 0 0 :image image)
+    (configure canvas :width 1600)
+    (configure canvas :height 900)
+    (pack canvas)
+    (loop
+      when (probe-file "active.lck") do
+          (loop-finish)
+      (sleep 1))
+    (exit-wish)))
 
-(defun main ()
-  (bt:make-thread #'show-idle-window :name "window")
-  (loop
-    (if (probe-file "active.lck")
-      (interrupt-thread-by-ame "window"))))
+(defun build-exe ()
+  (sb-ext:save-lisp-and-die "lock-window.exe" :toplevel #'show-idle-window
+                                              :executable t
+                                              :application-type :gui))
