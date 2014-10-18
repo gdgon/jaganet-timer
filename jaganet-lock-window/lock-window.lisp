0d7c1b5ca036a8b6cb26f7adbdc2c3c977672d56
Gerry Gonzaga <gdgonzaga@gmail.com>
2014-10-18 23:41:53 +0800

    Lock window now handles any bg.gif size, centers the window.

--- jaganet-lock-window/lock-window.lisp
+++ jaganet-lock-window/lock-window.lisp
@@ -1,26 +1,31 @@
 (ql:quickload 'ltk)
 (use-package 'ltk)
-(ql:quickload 'bordeaux-threads)
+(ql:quickload 'skippy)
 
 (defun show-idle-window ()
   (ignore-errors
     (start-wish)
-    (set-wm-overrideredirect *tk* 1)
-    (set-geometry-xy *tk* 0 0)
-    (defparameter image (make-image))
-    ;(let ((image (make-image)))
-    (image-load image "bg.gif")
-    (defparameter canvas (make-instance 'canvas))
-      ;(let ((canvas (make-instance 'canvas)))
-    (create-image canvas 0 0 :image image)
-    (configure canvas :width 1600)
-    (configure canvas :height 900)
-    (pack canvas)
-    (loop
-      when (probe-file "active.lck") do
-          (loop-finish)
-      (sleep 1))
-    (exit-wish)))
+    (let* ((img (skippy:load-data-stream "bg.gif"))
+           (img-width (skippy:width img))
+           (img-height (skippy:height img))
+           (win-x (truncate (- (/ (screen-width) 2) (/ img-width 2))))
+           (win-y (truncate (- (/ (screen-height) 2) (/ img-height 2)))))
+
+      (on-close *tk* (lambda () (format t "Closed")))
+      (set-wm-overrideredirect *tk* 1)
+      (set-geometry *tk* img-width img-height win-x win-y)
+      (defparameter image (make-image))
+      (image-load image "bg.gif")
+      (defparameter canvas (make-instance 'canvas))
+      (create-image canvas 0 0 :image image)
+      (configure canvas :width img-width)
+      (configure canvas :height img-height)
+      (pack canvas)
+      (loop
+        when (probe-file "active.lck") do
+            (loop-finish)
+        (sleep 1))
+      (exit-wish))))
 
 (defun build-exe ()
   (sb-ext:save-lisp-and-die "lock-window.exe" :toplevel #'show-idle-window
